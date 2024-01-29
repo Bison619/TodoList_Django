74076bf9f1f25151b687519debdc08b63b9b2b5b
Aman Baruwal <baruwal20@gmail.com>
2024-01-29 19:44:14 +0545

    db update

--- Home/admin.py
+++ Home/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from Home.models import Task
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display= ('tasktitle','time')
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task,TaskAdmin)
