dc8531f26dd9b1963d9f645179a6ca9b91072dfa
Aman Baruwal <baruwal20@gmail.com>
2024-01-29 19:35:56 +0545

    db insert

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class Task(models.Model):
+    tasktitle = models.TextField(max_length=40)
+    taskdesc = models.CharField( max_length=500)
+    time = models.DateTimeField( auto_now_add= True)
+
+    def __str__(self):
+        return self.tasktitle
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from Home.models import Task
 
 def index(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        desc = request.POST['desc']
+        ins = Task(tasktitle=title, taskdesc=desc)
+        ins.save()
     return render(request,"index.html")
