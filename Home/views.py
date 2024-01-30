eb18b19c6937961a499821e77e249adea591ac44
Aman Baruwal <baruwal20@gmail.com>
2024-01-30 20:44:26 +0545

    major issue in the for loop and if statement

--- Home/views.py
+++ Home/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from Home.models import Task
 
 def index(request):
+    alltask = Task.objects.all()
+    context = {'success': False , 'tasks' : alltask  }
     if request.method == "POST":
         title = request.POST['title']
         desc = request.POST['desc']
         ins = Task(tasktitle=title, taskdesc=desc)
         ins.save()
-    return render(request,"index.html")
+        context = {'success': True }
+    return render(request,"index.html" , context)
